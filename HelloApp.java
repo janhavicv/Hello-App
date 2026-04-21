314960fdf6f375f5b8d9170b28437a1de58d829b
janhavicv <janhavicv25@gmail.com>
2026-04-21 10:38:50 +0530

    Display hello using string

--- HelloApp.java
+++ HelloApp.java
@@ -1,18 +1,13 @@
 public class HelloApp {
     public static void main(String[] args) {
 
-        StringBuilder sb = new StringBuilder();
+        String s;
 
         if (args.length == 0) {
-            System.out.println("Hello, World!");
-            return;
+            s = "World";
+        } else {
+            s = String.join(", ", args);
         }
-
-        for (String n : args) {
-            sb.append(n).append(", ");
-        }
-
-        String s = sb.substring(0, sb.length() - 2);
 
         System.out.println("Hello, " + s + "!");
     }
